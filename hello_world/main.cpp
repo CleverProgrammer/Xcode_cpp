4b2d104f557febca2d98fd803f41472ad12f1eb8
rafeh01 <rafehqazi1@gmail.com>
2016-01-04 08:51:37 -0600

    added clang formatting with webkit standards to Xcode.

--- hello_world/main.cpp
+++ hello_world/main.cpp
@@ -8,25 +8,23 @@
 
 #include <iostream>
 
-
 void printStats()
 {
-    for (int i=0; i<10; i++)
-    {
+    for (int i = 0; i < 10; i++) {
         std::cout << i << std::endl;
     }
 }
 
-
 int main()
 {
     std::cout << "Hello, World!\n" << std::endl;
-    int arr[5] = {1, 2, 3, 4, 5};
+    int arr[5] = { 1, 2, 3, 4, 5 };
     int* arrPtr = arr;
     std::cout << arrPtr << std::endl;
     std::cout << *arrPtr << std::endl;
     arrPtr++;
     std::cout << *arrPtr << std::endl;
     // hello
+    printStats();
     return 0;
 }
