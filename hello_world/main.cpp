ad3fe4c9ef4ab8b3628b0624b3bb28d0df35c6f6
rafeh01 <rafehqazi1@gmail.com>
2016-01-04 07:12:30 -0600

    Initial Commit

---
+++ hello_world/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello_world
+//
+//  Created by Rafeh Qazi on 1/4/16.
+//  Copyright (c) 2016 ChessTastic. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
