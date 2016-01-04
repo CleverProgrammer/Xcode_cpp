e9a1caa5116db369621d6bb7a5b567e42d74b570
rafeh01 <rafehqazi1@gmail.com>
2016-01-04 07:46:54 -0600

    added printStats function and also added a cout statement to main.

--- hello_world/main.cpp
+++ hello_world/main.cpp
@@ -8,8 +8,18 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
+
+void printStats()
+{
+    for (int i=0; i<10; i++)
+    {
+        std::cout << i << std::endl;
+    }
+}
+
+
+int main()
+{
     std::cout << "Hello, World!\n";
     return 0;
 }
